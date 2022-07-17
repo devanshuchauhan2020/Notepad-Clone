a5212c55da21c44d080e2acfb9dbfbaa40c4b09a
devanshuchauhan2020 <71084898+devanshuchauhan2020@users.noreply.github.com>
2022-07-17 22:55:59 +0530

    Add files via upload

---
+++ NotepadMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.notepad;
+
+
+
+/**
+ *
+ * @author devanshuchauhan
+ */
+public class NotepadMain {
+    public static void main(String[] args){
+        NotePad note = new NotePad();
+    }
+            
+}
